81caae9b7fe3ead0c2775afd8106bfa2490d29ca
juanesgv <juanesgarviedma23@gmail.com>
2024-02-26 14:53:50 -0500

    Creación de usuario, hasheo de contraseña, validación de campo con espress-validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,27 @@
+import Usuario from "../models/Usuario.js"
+import { validationResult } from "express-validator"
 
-export const nuevoUsuario = async () =>{
-    console.log("desde nuevo usuario")
+export const nuevoUsuario = async (req, res) =>{
+
+    //mostar mensaje de error de express validator
+    const errores = validationResult(req)
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores : errores.array()})
+    }
+
+    const {email} = req.body
+    const existeUsuario = await Usuario.findOne({ email });
+
+    if(existeUsuario){
+        return res.status(400).json({msg: "El usuario ya está registrado"})
+    }
+
+    const usuario = new Usuario(req.body)
+    try {        
+        await usuario.save()
+        res.json({msg:"Usuario creado exitosamente"})
+    } catch (error) {
+        console.log(error)
+    }
+
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import { nuevoUsuario } from '../controllers/usuarioController.js'
+import { check } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/', nuevoUsuario )
+router.post('/', 
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email valido').isEmail(),
+        check('email', 'La contraseña debe tener mínimo 6 caracteres').isLength({ min: 6}),
+    ], 
+    nuevoUsuario )
 
 export default router
